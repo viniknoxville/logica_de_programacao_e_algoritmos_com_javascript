b85f14f3d564fc71b4bc5c3e5f459a60f4a77c8a
viniknoxville <ass.marcosribeiro@gmail.com>
2025-03-23 21:41:56 -0300

    exemplo 3

---
+++ Capitulo_03/js/ex3_2.js
@@ -0,0 +1,52 @@
+function calcularPeso() {
+    // cria refeência aos elementos manipulados pela function
+    var inNome = document.getElementById("inNome");
+    var rbMasculino = document.getElementById("rbMasculino");
+    var rbFeminino = document.getElementById("rbFeminino");
+    var inAltura = document.getElementById("inAltura");
+    var outResposta = document.getElementById("outResposta");
+
+    // obtem o conteudo dos campos de edição da página
+    var nome = inNome.value;
+    var masculino = rbMasculino.checked;
+    var feminino = rbFeminino.checked;
+    var altura = Number(inAltura.value);
+
+    // verifica se o nome foi preenchido e o sexo selecionado
+    if (nome == "" || (masculino == false && feminino == false)) {
+        alert("Por favor, informe o nome e selecione o gênero...");
+        inNome.focus(); //posiciona (joga foco) no campo de edição inNome
+        return;
+    }
+
+    // se altura vazio (0) ou NaN:Not a Number (um texto for informado por exemplo)
+    if (altura == 0 || isNaN(altura)) {
+        alert("Por favor, informe a altura corretamente!");
+        inAltura.focus();
+        return;
+    }
+
+    // se masculino 
+    if (masculino) {
+        var peso = 22 * Math.pow(altura, 2); // Math.pow eleva ao quadrado
+    } else {
+        var peso = 21 * Math.pow(altura, 2);
+    }
+
+    //apresenta a resposta (altera o conteudo da linha outResposta)
+    outResposta.textContent = nome + ": seu peso ideal é: " + peso.toFixed(3) + " kg";
+}
+
+var btCalcular = document.getElementById("btCalcular");
+btCalcular.addEventListener("click", calcularPeso);
+
+function limparCampos(){
+    location.reload();
+    document.getElementById("inNome").focus;
+}
+
+
+
+var btLimpar = document.getElementById("btLimpar");
+btLimpar.addEventListener("click", limparCampos);
+
